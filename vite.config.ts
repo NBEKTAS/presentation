2cae083cfaecda9c86298de004ff1041fa4e81b8
NBEKTAS <49793884+NBEKTAS@users.noreply.github.com>
2026-09-16 12:23:35 +0200

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/presentation/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
